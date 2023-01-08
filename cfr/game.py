e9cc7e59b6ef8c8d4ba1f56fc3edda82c4937ff2
Pascal <ppxscal@gmail.com>
2023-01-08 14:36:13 -0500

    abstract game class

---
+++ cfr/game.py
@@ -0,0 +1,35 @@
+
+class Game:
+
+    def beginGame():
+        '''Begins a new Game'''
+        pass
+
+    def makeMove():
+        '''Makes a move for the player'''
+        pass
+    
+    def getPayout():
+        '''Return a payout vector for a finished game'''
+    
+    def deepCopy():
+        '''Returns a deep copy of the game object'''
+        pass
+
+    def getInfoSet():
+        '''Returns the information set hash for the current player'''
+        pass
+
+    def isTerminal():
+        '''Returns True if the game is finished'''
+        pass
+
+    def getPlayer():
+        '''Returns the current player'''
+        pass
+
+    def getActions():
+        '''Returns a list of all possible actions'''
+        pass
+
+
